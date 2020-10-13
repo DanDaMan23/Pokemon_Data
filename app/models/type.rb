2d64dffa12f1d51c9dfd6b2ed365730357de731f
DanDaMan23 <danlawrcons@gmail.com>
2020-10-13 16:48:33 -0500

    Connected Types and Pokemon. Also created a controller for Type.

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,3 @@
 class Type < ApplicationRecord
-    # has_and_belongs_to_many :pokemon
+    has_and_belongs_to_many :pokemons
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'types/index'
+  get 'types/show'
   get 'pokemons/index'
   get 'pokemons/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
