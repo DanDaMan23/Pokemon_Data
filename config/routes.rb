4a48bd0efd6f1bf363aabdc8f0c62b61047ee1a8
DanDaMan23 <danlawrcons@gmail.com>
2020-10-15 01:32:51 -0500

    Created a homepage and a NavBar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # get 'types/index'
   # get 'types/show'
   # get 'pokemons/index'
   # get 'pokemons/show'
 
 
-  root to: 'pokemons#index'
+  root to: 'pages#home'
   resources "pokemons", only: %i[index show]
   resources "types", only: %i[index show]
 
